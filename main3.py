d1d8a800ff831e15a1925b882ce6f1241e10ced5
seakyith <seakyith@gmail.com>
2021-11-30 20:11:04 -0800

    pull from the source

--- main3.py
+++ main3.py
@@ -43,7 +43,8 @@ print("\nInsertion sort = 1\n")
 print("Selection Sort = 2\n")
 print("Shellsort = 3\n")
 print("Bubblesort = 4\n")
-print("QuickSort 3 Way = 5\n")
+print("Mergesort = 5\n")
+print("Quicksort = 6\n")
 m=int(input("Enter a sort type\n"))
 
 
@@ -109,49 +110,57 @@ def bubblesort(a):
                 yield a
                 a[j],a[j+1] = a[j+1],a[j]
                 yield a
-
-# Quick Sort 3-Way
-def quicksort3(a):
-    f =  0
-    l = len(a) - 1
-    quicksort3Helper(a, f, l)
-
-def swap (a, i , j):
-    temp = a[i]
-    a[i] = a[j]
-    a[j] = temp
-#Quick Sort 3 -way helper function
-def quicksort3Helper(a, f , l):
-    if (f >= l):
-        return
-    if (f - l == 1):
-        if a[f] < a[l]:
-            swap(a, f, l)
-        return
     
-    x, y = quicksort3Partition(a, f, l)
-    quicksort3Helper(a, f, x)
-    quicksort3Helper(a, y, l)
-#quicksort 3 way partition
-def quicksort3Partition(a, f, l):
-    mid = f
-    pivot = a[l]
+# Merge sort
+def mergeSort(a):
+    size = len(a)
+    result = []
+    for i in range(size):
+        result.append([])
+        result[i].append(a[i])
 
-    while mid <= l:
-        if a[mid] < pivot:
-            swap(a, f, mid)
-            f += 1
-            mid += 1
-        elif a[mid]  > pivot:
-            swap(a, mid, l)
-            l -=1
+    while size > 1:
+        i = 0
+        j = 0
+        temp = []
+        while i < size // 2:
+            temp.append([])
+            temp[i].extend(result[j])
+            temp[i].extend(result[j + 1])
+            temp[i].sort()
+            yield sum(temp,[])
+            i += 1
+            j += 2
+            
+        if size % 2 == 1:
+            temp.append([])
+            temp[i].extend(result[j])
+            yield sum(temp,[])
+            size = size // 2 + 1
         else:
-            mid +=1
-    return f - 1, mid
+            size = size // 2
 
-  
+        result = list(temp)
+        yield sum(result,[])
 
-          
+
+#Quicksort
+def quicksort(a, l ,r):
+    if l >= r:
+        return
+    x = a[l]
+    j = l
+    for i in range(l + 1, r + 1):
+        if a[i] <= x:
+            j += 1
+            a[j], a[i] = a[i], a[j]
+        yield a
+    a[l], a[j] = a[j], a[l]
+    yield a
+
+    yield from quicksort(a, l, j - 1)
+    yield from quicksort(a, j + 1, r)
+
 
 # generator object returned by the function
 if m==1:
@@ -166,11 +175,13 @@ if m==3:
 if m==4:
     generator = bubblesort(a)
     plottype = "Running Bubble Sort"
-if m==5:
-    generator = quicksort3(a)
-    plottype = "Running Quick Sort 3 Ways"
-
-generator == shellsort(a)
+    #generator == shellsort(a)
+if m == 5:
+    generator = mergeSort(a)
+    plottype = "Running Merge Sort"
+if m == 6:
+    generator = mergeSort(a)
+    plottype = "Running QuickSort Sort"
 
 # to set the colors of the bars.
 data_normalizer = mp.colors.Normalize()
@@ -211,6 +222,7 @@ def animate(A, rects, iteration):
     # to the value of the elements
     for rect, val in zip(rects, A):
         rect.set_height(val)
+
     iteration[0] += 1
     text.set_text("iterations : {}".format(iteration[0]))
 
@@ -225,4 +237,3 @@ ax.set_title(plottype)
 fig.suptitle(str(n) + " Data Points", fontsize=12)
 
 plt.show()
-
